b43304944e0d89fe169f82d3359b23864213db0d
Frank Ocrospoma Ugaz <frankocrospomaugaz@gmail.com>
2024-02-14 12:16:22 -0500

    weaaa

--- bd.py
+++ bd.py
@@ -2,6 +2,7 @@ import pymysql
 
 def obtener_conexion():
     return pymysql.connect(host='viaduct.proxy.rlwy.net',
+                                port=34430,
                                 user='root',
                                 password='aH2gEHhFde5GGHDA-6HdfD42bd-2bb43',
                                 db='tienda')
