ff70a2ba37d68c6676d46e865dc93ae5fb118e52
Jeancarlos Chafloque Velásquez <jc_26.10@hotmail.com>
2024-02-24 15:07:03 -0500

    Add requrements.txt

--- bd.py
+++ bd.py
@@ -1,7 +1,11 @@
 import pymysql
 
 def obtener_conexion():
-    return pymysql.connect(host='Grupo04DAW.mysql.pythonanywhere-services.com',
-                                user='Grupo04DAW',
-                                password='DAW_2023_2_TIENDA',
-                                db='Grupo04DAW$tienda')
+    # return pymysql.connect(host='Grupo04DAW.mysql.pythonanywhere-services.com',
+    #                             user='Grupo04DAW',
+    #                             password='DAW_2023_2_TIENDA',
+    #                             db='Grupo04DAW$tienda')
+        return pymysql.connect(host='localhost',
+                                user='root',
+                                password='',
+                                db='tienda')
